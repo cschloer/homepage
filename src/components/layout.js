6331128cb90073932e5ce71b4a608d8ce1218a0f
Conrad Schloer <conrad.schloer@gmail.com>
2019-02-26 20:43:37 +0100

    Add reactstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,43 +1,47 @@
-import React from "react"
+import React, { Component } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+
+class Layout extends Component {
+
+  state = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+        `}
+        render={data => (
+          <>
+            <Header />
+            <div>
+              <main>{this.props.children}</main>
+            </div>
+          </>
+        )}
+      />
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,65 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    Jumbotron,
+    Button
+} from 'reactstrap';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class App extends Component {
 
-export default IndexPage
+  state = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <Jumbotron>
+          <Container>
+              <Row>
+                  <Col>
+                      <h1>Welcome to React</h1>
+                      <p>
+                          <Button
+                              tag="a"
+                              color="success"
+                              size="large"
+                              href="http://reactstrap.github.io"
+                              target="_blank"
+                          >
+                              View Reactstrap Docs
+                          </Button>
+                      </p>
+                  </Col>
+              </Row>
+          </Container>
+        </Jumbotron>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    );
+  }
+}
+
+export default App;
